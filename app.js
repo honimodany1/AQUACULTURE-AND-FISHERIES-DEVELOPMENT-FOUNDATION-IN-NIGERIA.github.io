21ca0a75b0137e637fb0579c836e9c9b9f7342ad
honimodany1 <104818517+honimodany1@users.noreply.github.com>
2022-05-03 22:23:33 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0"
+}
+
+function hideMenu() {
+    navLinks.style.rigth = "-200px";
+}
+
+
+const form = document.querySelector('signup-form');
+
+form.addEventListener('submit', e => {
+
+})
